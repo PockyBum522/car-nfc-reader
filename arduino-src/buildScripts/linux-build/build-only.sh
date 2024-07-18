3aff79e4e6464bbdb5981e4179b6b457897ce7f5
DSikes <pockybum522@gmail.com>
2024-07-18 09:56:20 -0400

    Add linux build script

--- arduino-src/buildScripts/linux-build/build-only.sh
+++ arduino-src/buildScripts/linux-build/build-only.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-PROJECT_BASE_PATH="/media/secondary/repos/kit-contracting-work/non-contracted work/esp32-s3-small-rotovap-controller"
-
-cd "$PROJECT_BASE_PATH" || return
-
-BUILD_BASE_PATH="$PROJECT_BASE_PATH/.pio/build/esp32-s3-devkitc-1"
-
-FIRMWARE_PATH="$BUILD_BASE_PATH/firmware.bin"
-
-echo ""
-echo "For project in:"
-echo "$PROJECT_BASE_PATH"
-echo ""
-echo "Built:"
-echo "$FIRMWARE_PATH"
-echo ""
-echo "In:"
-echo "$BUILD_BASE_PATH"
-echo ""
+cd ../..
 
 pio run
 
-echo ""
+cd .pio\build\esp32-s3-devkitc-1\
+
+nemo . &
+
+echo "Drag firmware.bin onto the ElegantOTA page"
+
+firefox "http://192.168.1.111/update"
+
+cd ../../../buildScripts/linux-build
